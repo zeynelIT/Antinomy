1e52b84baf2380f7ff4c4582e786a219dab32b9b
RonaldDomi <ronalddomi4@gmail.com>
2023-04-27 15:53:44 +0200

    implementation echangeAvecMain

--- src/Modele/Continuum.java
+++ src/Modele/Continuum.java
@@ -14,31 +14,35 @@ public class Continuum {
 	Carte getCarteContinuum(int index){
 		return this.continuum.get(index);
 	}
+	int getContinuumSize(){
+		return continuum.size();
+	}
 
 	void setCarteContinuum(int index, Carte carte){
 		this.continuum.set(index, carte);
 	}
-	
-	int getContinuumSize(){
-		return continuum.size();
-	}
-	
 
-	
+	Carte echangeCarteContinuum(int index, Carte carte){
+		Carte res = continuum.get(index);
+		setCarteContinuum(index, carte);
+		return res;
+	}
+
+
 	/**
 	 * TODO: tester fonction
 	 * <P>
-	* <BR> Renvoie les déplacements possibles pour toutes les cartes en main.
-	* <BR> L'élément renvoyé est un tableau de taille 3 contenant un sous tableau de tous les
-	*  indices valides pour un déplacement.
-	*
-	* @param cartes Une liste correspondant aux 3 cartes en main
-	* @param indice_sorcier l'index du sorcier sur le Continuum
-	* <P>
-	* @return Un tableau de 3 tableaux d'entiers où tab[0] est le tableau des déplacements possibles
-	* à partir de cartes[0]
-	*
-	*/
+	 * <BR> Renvoie les déplacements possibles pour toutes les cartes en main.
+	 * <BR> L'élément renvoyé est un tableau de taille 3 contenant un sous tableau de tous les
+	 *  indices valides pour un déplacement.
+	 *
+	 * @param cartes Une liste correspondant aux 3 cartes en main
+	 * @param indice_sorcier l'index du sorcier sur le Continuum
+	 * <P>
+	 * @return Un tableau de 3 tableaux d'entiers où tab[0] est le tableau des déplacements possibles
+	 * à partir de cartes[0]
+	 *
+	 */
 	int[][] calculeOptions(List<Carte> cartes, int indice_sorcier){
 		int[][] tab_options = new int[cartes.size()][];
 		for (int i = 0; i < cartes.size(); i++) {
@@ -50,44 +54,50 @@ public class Continuum {
 		}
 		return tab_options;
 	}
-	
+
 	/**
-	* <P> TODO: tester fonction </P>
-	*
-	* <P> Renvoie les déplacements possibles suivant la carte jouée et l'index du sorcier. Peu importe la direction.
-	* <BR>  L'élément renvoyé est une liste chaînée d'index de cartes où le déplacement est valide.
-	* <BR> Il existe toujours au moins 1 déplacement possible. </P>
-	*
-	* <P>
-    * @param carte_jouee La carte jouée durant ce tour
-	* @param num_sorcier L'index du sorcier sur le Continuum
-	* @param direction La direction où le sorcier doit se déplacer pour aller dans le Futur (-1 ou +1)
-	* </P>
-	* <P>
-	* @return Une liste chaînée d'index valides.
-	* </P>
-	*/
+	 * <P> TODO: tester fonction </P>
+	 *
+	 * <P> Renvoie les déplacements possibles suivant la carte jouée et l'index du sorcier. Peu importe la direction.
+	 * <BR>  L'élément renvoyé est une liste chaînée d'index de cartes où le déplacement est valide.
+	 * <BR> Il existe toujours au moins 1 déplacement possible. </P>
+	 *
+	 * <P>
+	 * @param carte_jouee La carte jouée durant ce tour
+	 * @param num_sorcier L'index du sorcier sur le Continuum
+	 * @param direction La direction où le sorcier doit se déplacer pour aller dans le Futur (-1 ou +1)
+	 * </P>
+	 * <P>
+	 * @return Une liste chaînée d'index valides.
+	 * </P>
+	 */
 	public LinkedList<Integer> getCoupsPossibles(Carte carte_jouee, int num_sorcier, int direction){
 		LinkedList<Integer> index_coups_possibles = new LinkedList<Integer>();
-		
+
 		for (int i = num_sorcier-direction; i >= 0 && i < this.getContinuumSize(); i-= direction){
 			if(this.getCarteContinuum(i).getSymbole() == carte_jouee.getSymbole() ||
 					this.getCarteContinuum(i).getCouleur() == carte_jouee.getCouleur()){
 				index_coups_possibles.add(i);
 			}
 		}
-		
+
 		if (((num_sorcier + (carte_jouee.getNumero() * direction)) < this.getContinuumSize()) ||
 				(num_sorcier + (carte_jouee.getNumero() * direction)) >= 0){
 			index_coups_possibles.add(num_sorcier + (carte_jouee.getNumero() * direction));
 		}
 		return index_coups_possibles;
 	}
-	
-	
-	void echangeAvecMain(List<Carte> main, int indice_sorcier){
-		// todo
+
+	List<Carte> echangeAvecMain(List<Carte> main, int indiceSorcier, boolean droite){
 		// prend comme argument les 3 cartes du main
-		// renvoie les 3 cartes echanges
+		// renvoie les 3 cartes echanges a partir d'indice de sorcier dans le direction
+		List<Carte> res = new ArrayList<>();
+		for(int i = 1; i <= 3; i++){
+			int indiceCarte = droite ? indiceSorcier + i : indiceSorcier - i;
+			Carte carteCourantContinuum = echangeCarteContinuum(indiceCarte, main.get(i-1));
+			res.add(carteCourantContinuum);
+		}
+
+		return res;
 	}
 }
